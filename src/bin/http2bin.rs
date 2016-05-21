5ac5b5d67e3481084cd011e3e9ff057c3f28638d
FUJI Goro (gfx) <gfuji@cpan.org>
2016-05-21 12:12:18 +0900

    read timeout / write timeout

--- src/bin/http2bin.rs
+++ src/bin/http2bin.rs
@@ -1,10 +1,17 @@
 extern crate hyper;
 
+fn create_client() -> hyper::Client {
+    let mut client = hyper::Client::new();
+    client.set_read_timeout(Some(std::time::Duration::from_millis(1000)));
+    client.set_write_timeout(Some(std::time::Duration::from_millis(1000)));
+    return client;
+}
+
 fn main() {
     let url = "https://http2bin.org/get";
     let ua = "rust-sandbox/1.0".to_string();
 
-    let client = hyper::Client::new();
+    let client = create_client();
     let request_builder = client.get(url)
         .header(hyper::header::UserAgent(ua));
     let mut res = request_builder.send().unwrap();
