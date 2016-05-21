8078a0fd775d976cdc783cbcee54d7b1e38a838a
FUJI Goro (gfx) <gfuji@cpan.org>
2016-05-21 11:50:44 +0900

    set a header in hyper

--- src/bin/http2bin.rs
+++ src/bin/http2bin.rs
@@ -1,10 +1,13 @@
 extern crate hyper;
 
-static URL: &'static str = "https://http2bin.org/get";
-
 fn main() {
+    let url = "https://http2bin.org/get";
+    let ua = "rust-sandbox/1.0".to_string();
+
     let client = hyper::Client::new();
-    let mut res = client.get(URL).send().unwrap();
+    let request_builder = client.get(url)
+        .header(hyper::header::UserAgent(ua));
+    let mut res = request_builder.send().unwrap();
 
     println!("Response: {}", res.status);
     println!("Headers:\n{}", res.headers);
